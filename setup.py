8c9b92081322fd2bc1a6df60a6c89b6b5e33f5e9
Cedric Abunar <cabunar@wazo.io>
2018-02-26 13:41:22 -0500

    fix python project name with - instead of _

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
 
-PROJECT = 'wazo-admin-ui-call-filter'
+PROJECT = 'wazo_admin_ui_call_filter'
 AUTHOR = 'Wazo Authors'
 EMAIL = 'dev@wazo.community'
 
